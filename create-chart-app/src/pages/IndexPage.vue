ee007b4c6c5f481099a78bf385c1e655074f6da9
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-08-28 17:39:09 +0700

    create chart app

---
+++ create-chart-app/src/pages/IndexPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+    <div>sdfgds
+      <canvas ref="chart" width="400" height="400"></canvas>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  mounted() {
+    this.renderChart();
+  },
+
+  methods: {
+    renderChart() {
+      const ctx = this.$refs.chart.getContext('2d');
+      new MChart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'red',
+              'blue',
+              'yellow',
+              'green',
+              'purple',
+              'orange',
+            ],
+            borderWidth: 1,
+          }],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    },
+  },
+})
+</script>
